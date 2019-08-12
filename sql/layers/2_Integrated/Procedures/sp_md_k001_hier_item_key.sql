6b501fa7e84b292c9b84e34bcc98588bfed8b43b
swiernia <g.swierniak.hcs@gmail.com>
2019-08-12 09:04:36 +0000

    changed natural key for hier_item_id

--- sql/layers/2_Integrated/Procedures/sp_md_k001_hier_item_key.sql
+++ sql/layers/2_Integrated/Procedures/sp_md_k001_hier_item_key.sql
@@ -10,18 +10,15 @@ insert into
     src_syst_id
   )
 select
-  row_number() over( order by a.node_id) + 1000000 as hier_item_id,
-  'Product_Group' as hier_item_src_pfx,
-  a.node_id as hier_item_src_key,
-  1 as src_syst_id --source code for master data
+  row_number() over( order by w_002.hier_item_src_key) + 1000000 as hier_item_id,
+  hier_item_src_pfx,
+  hier_item_src_key,
+  src_syst_id --source code for master data
 from
-  demo_sl.products_tree_unq a
-  left join
-    demo_il.k001_hier_item_key k001
-    on cast(a.node_id as varchar(255)) = k001.hier_item_src_key
-    and k001.hier_item_src_pfx = 'Product_Group'
+  demo_il.w_002_hier_item w_002
+
 where
-  k001.hier_item_id is null
+  w_002.hier_item_id is null
 ;
 END
 $$;

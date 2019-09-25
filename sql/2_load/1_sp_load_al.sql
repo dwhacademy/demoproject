90df2a5e83dc49cd072f3f53de4fa5b584775195
swiernia <g.swierniak.hcs@gmail.com>
2019-09-25 23:45:48 +0200

    issue-9: implemented fact_orders

---
+++ sql/2_load/1_sp_load_al.sql
@@ -0,0 +1,14 @@
+BEGIN TRANSACTION;
+CREATE OR REPLACE PROCEDURE dev_demo_al.sp_load_al()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+
+call dev_demo_al.sp_fact_orders();
+
+
+END
+$$;
+CALL dev_demo_ml.sp_deployment_objects('sp_load_al', 'dev_demo_al');
+END TRANSACTION;
+

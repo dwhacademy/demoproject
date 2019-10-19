80578bb1eecdf4bd1cc633782c80238feb468d5a
Lubomir Kamensky <lubomir.kamensky@gmail.com>
2019-10-19 22:12:53 +0200

    issue-12: work in progress

--- sql/2_load/1_sp_load_al.sql
+++ sql/2_load/1_sp_load_al.sql
@@ -4,7 +4,6 @@ LANGUAGE plpgsql
 AS $$
 DECLARE Load_ID INTEGER;
 DECLARE Status VARCHAR(255) DEFAULT 'Success';
-DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET Status = 'Failure';
 BEGIN
 
 -- LOAD INIT.
@@ -12,7 +11,13 @@ SET Status = 'Success';
 SELECT COALESCE(MAX(load_id)+1,100000) into Load_ID  FROM dev_demo_ml.load_stat WHERE proc_nm = 'LOAD_INIT'; 
 INSERT INTO dev_demo_ml.load_stat VALUES(:Load_ID, 'LOAD_INIT', CURRENT_TIMESTAMP, 'AL', :Status);
 
-call dev_demo_al.sp_dim_prod_hier();
+BEGIN
+  call dev_demo_al.sp_dim_prod_hier();
+EXCEPTION WHEN OTHERS THEN
+  Status = 'Failure';
+END;
+
+
 call dev_demo_al.sp_dim_customer();
 call dev_demo_al.sp_dim_employee();
 call dev_demo_al.sp_dim_store();

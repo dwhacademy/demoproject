9267c68a6f64630d2479fe4e4d10e6837e25b267
Lubomir Kamensky <lubomir.kamensky@gmail.com>
2019-10-19 22:14:56 +0200

    issue-12: work in progress

--- sql/2_load/1_sp_load_al.sql
+++ sql/2_load/1_sp_load_al.sql
@@ -9,7 +9,7 @@ BEGIN
 -- LOAD INIT.
 SET Status = 'Success';
 SELECT COALESCE(MAX(load_id)+1,100000) into Load_ID  FROM dev_demo_ml.load_stat WHERE proc_nm = 'LOAD_INIT'; 
-INSERT INTO dev_demo_ml.load_stat VALUES(:Load_ID, 'LOAD_INIT', CURRENT_TIMESTAMP, 'AL', :Status);
+INSERT INTO dev_demo_ml.load_stat VALUES(Load_ID, 'LOAD_INIT', CURRENT_TIMESTAMP, 'AL', Status);
 
 BEGIN
   call dev_demo_al.sp_dim_prod_hier();

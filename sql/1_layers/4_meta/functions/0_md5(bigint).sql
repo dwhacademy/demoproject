fbb98023062c1b68d3f5327dae6af7734478412e
swiernia <g.swierniak.hcs@gmail.com>
2019-11-05 22:44:14 +0100

    issue-11: renamed function md5 to md5_hash

--- sql/1_layers/4_meta/functions/0_md5(bigint).sql
+++ sql/1_layers/4_meta/functions/0_md5(bigint).sql
@@ -1,4 +1,4 @@
 create or replace function md5_hash(bigint)
 returns text as $$
-select coalesce(($1::text),md5(1));
+select ($1::text)||'x';
 $$ language sql immutable;
--- sql/1_layers/4_meta/functions/0_md5(timestamp without time zone).sql
+++ sql/1_layers/4_meta/functions/0_md5(timestamp without time zone).sql
@@ -1,4 +1,4 @@
 create or replace function md5_hash(timestamp without time zone)
 returns text as $$
-select coalesce(($1::text),md5(1));
+select ($1::text)||'x';
 $$ language sql immutable;

3edfafb7a6cdac8904dde3008481e200d829c372
swiernia <g.swierniak.hcs@gmail.com>
2019-11-13 21:49:39 +0100

    issue-11: replaced md5 with md5_hash

--- sql/1_layers/4_meta/functions/0_md5_hash(bigint).sql
+++ sql/1_layers/4_meta/functions/0_md5_hash(bigint).sql
@@ -1,4 +1,4 @@
 create or replace function md5_hash(bigint)
 returns text as $$
-select ($1::text)||'x';
+select coalesce(md5($1::text),'x');
 $$ language sql;
--- sql/1_layers/4_meta/functions/0_md5_hash(varchar).sql
+++ sql/1_layers/4_meta/functions/0_md5_hash(varchar).sql
@@ -1,4 +1,4 @@
 create or replace function md5_hash(varchar)
 returns text as $$
-select ($1::text)||'x';
+select coalesce(md5($1::text),'x');
 $$ language sql;
